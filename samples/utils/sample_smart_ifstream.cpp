2ac1dace2327b42f24485ac98c4f6f60f6d32b0a
RingZ <RingZEROtlf@outlook.com>
2020-03-30 12:40:48 +0800

    add Makefile in samples  - support rule for utils in samples

--- samples/utils/sample_smart_ifstream.cpp
+++ samples/utils/sample_smart_ifstream.cpp
@@ -4,7 +4,7 @@
 
 int main() {
   using ifstream = utils::io::smart_ifstream;
-  ifstream in("../assets/dfa/dfa_define.txt");
+  ifstream in("assets/dfa/dfa_define.txt");
   std::size_t num_states, num_finalize_states;
   in >> num_states >> num_finalize_states;
   std::cout << num_states << " " << num_finalize_states << "\n";
