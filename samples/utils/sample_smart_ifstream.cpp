999cabd6782ca0690772122cbf65d67937b700e9
RingZ <RingZEROtlf@outlook.com>
2020-04-09 13:53:41 +0800

    update sample_smart_ifstream.cpp  - use argv[1] as input  - update CMakeLists.txt

--- samples/utils/sample_smart_ifstream.cpp
+++ samples/utils/sample_smart_ifstream.cpp
@@ -2,22 +2,22 @@
 
 #include <utils/io/smart_ifstream.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
   using ifstream = utils::io::smart_ifstream;
-  ifstream in("assets/dfa/dfa_define.txt");
+  ifstream in_stream(argv[1]);
   std::size_t num_states, num_finalize_states;
-  in >> num_states >> num_finalize_states;
+  in_stream >> num_states >> num_finalize_states;
   std::cout << num_states << " " << num_finalize_states << "\n";
   while (num_finalize_states--) {
     std::size_t idx_state;
     std::string token_id;
-    in >> idx_state >> token_id;
+    in_stream >> idx_state >> token_id;
     std::cout << idx_state << " " << token_id << "\n";
   }
   {
     std::size_t idx_state_in, idx_state_out;
     std::string parameter;
-    while (in >> idx_state_in >> idx_state_out >> parameter) {
+    while (in_stream >> idx_state_in >> idx_state_out >> parameter) {
       std::cout << idx_state_in << " -> " << idx_state_out << " " << parameter << "\n";
     }
   }
